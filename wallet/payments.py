a2b2b5d991bedcbc66336b04a5f7257bc6df504a
iradtaufique <iradukunda.ta@gmail.com>
2022-02-17 08:58:18 +0200

    remove commented snippets from payments.py

--- wallet/payments.py
+++ wallet/payments.py
@@ -33,37 +33,3 @@ def process_payment(name, amount):
     print('=============== response==============', response)
     link = response['data']['link']
     return link
-
-
-# def process_payment(name, amount):
-#     hed = {'Authorization': 'Bearer ' + config('API_SECRET_KEY')}
-#
-#     data = {
-#         "tx_ref": '' + str(math.floor(1000000 + random.random() * 9000000)),
-#         "amount": str(amount),
-#         "currency": "RWF",
-#         "redirect_url": "http://localhost:8000/wallet/callback",
-#         "payment_options": "mobilemoney",
-#         "network": "MTN",
-#         "meta": {
-#             "consumer_id": 23,
-#             "consumer_mac": "92a3-912ba-1192a"
-#         },
-#         "customer": {
-#             "email": 'fab.wastee@gmail.com',
-#             "phonenumber": "0783845574",
-#             "name": name
-#         },
-#         "customizations": {
-#             "title": "Saving Wallet",
-#             "description": "Adding Money To Saving Wallet",
-#             "logo": "https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg"
-#         }
-#     }
-#
-#     url = 'https://api.flutterwave.com/v3/payments'
-#     response = requests.post(url, json=data, headers=hed)
-#     response = response.json()
-#     print('=============== response==============',response)
-#     link = response['data']['link']
-#     return link
